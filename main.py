cebd899fcbd2be7281ab7277fc9c05d3bea124f8
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-18 16:17:11 +0530

    create Api to Scrape paragraph data from Provided Url

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, HttpUrl
+from scraper import fetch_data, extract_data
 
 app = FastAPI()
 
@@ -6,4 +8,19 @@ app = FastAPI()
 @app.get('/') 
 def scrape_data():
     return {'data': 'Hello! Scraping'}
+
+
+
+class ScraperRequest(BaseModel):
+    url : HttpUrl
+
+@app.post('/scrape')
+def scrape_data(request: ScraperRequest):
+    try:
+        soup = fetch_data(request.url)
+        data = extract_data(soup)
+        return {"data": data}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'An error occoured: {str(e)}')
 
--- scraper.py
+++ scraper.py
@@ -15,14 +15,9 @@ def fetch_data(url):
     soup = BeautifulSoup(response.content, "html.parser")
     return soup
 
-html_content= fetch_data(url)
+
 
 def extract_data(soup):
     paragraphs = soup.find_all('p')
     return [para.get_text() for para in  paragraphs]
 
-paragraphs = extract_data(html_content)
-
-for paragraph in paragraphs:
-    print(paragraph)
-    print("\n" + "-" * 50 + "\n")
