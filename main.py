53506b0a13e553d4f228511d812d1b5f0f6132fd
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-25 12:55:28 +0530

    Update main.py file with comments

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ from scraper import fetch_data, extract_data
 
 app = FastAPI()
 
-
+# Test endpoint for basic functionality
 @app.get('/') 
 def scrape_data():
     return {'data': 'Hello! Scraping'}
 
-
-
+# Request model for the POST request
 class ScraperRequest(BaseModel):
-    url : HttpUrl
+    url: HttpUrl  # The URL to scrape from, must be a valid HTTP URL
 
+# POST endpoint for scraping content from the given URL
 @app.post('/scrape')
 def scrape_data(request: ScraperRequest):
     try:
+        # Fetch and extract data using the provided URL
         soup = fetch_data(request.url)
         data = extract_data(soup)
-        return {"data": data}
-    
+        return {"data": data}  # Return the extracted data as a dictionary
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'An error occoured: {str(e)}')
-
+        # Return a 500 Internal Server Error if something goes wrong
+        raise HTTPException(status_code=500, detail=f'An error occurred: {str(e)}')
